ddfdf4ce3d657eaf8d98bbd4a0119f128eb64aac
Amir <amir.fayzullin@gmail.com>
2020-11-05 21:11:45 +0500

    14 lesson: css-modules

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,23 @@
 import React from 'react';
+import s from './Navbar.module.css';
 
 const Navbar = () => {
     return (
-        <nav className='nav'>
-            <div>
-                <a>
-                    Profile
-                </a>
+        <nav className={s.nav}>
+            <div className={`${s.item} ${s.active}`}>
+                <a>Profile</a>
             </div>
-            <div>
-                <a>
-                    Messages
-                </a>
+            <div className={s.item}>
+                <a>Messages</a>
             </div>
-            <div>
-                <a>
-                    News
-                </a>
+            <div className={s.item}>
+                <a>News</a>
             </div>
-            <div>
-                <a>
-                    Music
-                </a>
+            <div className={s.item}>
+                <a>Music</a>
             </div>
-            <div>
-                <a>
-                    Settings
-                </a>
+            <div className={s.item}>
+                <a>Settings</a>
             </div>
         </nav>
     );
