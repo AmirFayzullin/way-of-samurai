bcb591da5ccfe67e815d4b7d5e8a4ae522d03d2b
Amir <amir.fayzullin@gmail.com>
2020-11-08 23:16:09 +0500

    21 lesson: dialogs page layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={`${s.dialog}`}>
+                    <NavLink to='/dialogs/id_1' activeClassName={s.active}>John</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/id_2' activeClassName={s.active}>Mary</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/id_3' activeClassName={s.active}>Sam</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hi, bro</div>
+                <div className={s.message}>How are you?</div>
+            </div>
         </div>
     )
 };
